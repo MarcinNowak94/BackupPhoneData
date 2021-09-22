1881dd9bc058b9af069529027ebf00e8a8eae55a
Marcin Nowak <SourVenom1994@gmail.com>
2021-09-22 17:51:00 +0200

    initializing repo

---
+++ example-backup-script.ps1
@@ -0,0 +1,25 @@
+﻿$devicename="device_name"
+$storagedir='c:\Where\to\store\files'
+$getall='(.*)$'
+
+$something_to_backup_1 = [PSCustomObject]@{
+    source = "path\after\device_name"
+    destination = "$storagedir\where\to\put\it\1"
+    filter = $getall
+};
+$something_to_backup_2 = [PSCustomObject]@{
+    source = "path\after\device_name"
+    destination = "$storagedir\where\to\put\it\2"
+    filter = $getall
+};
+
+
+$Thingstobackup = New-Object System.Collections.ArrayList
+
+$Thingstobackup.Add($something_to_backup_1) | Out-Null
+$Thingstobackup.Add($something_to_backup_2) | Out-Null
+
+
+$Thingstobackup | ForEach-Object {
+    c:\script-location\Get-PhoneData.ps1 -phoneName $devicename -sourceFolder $_.source -targetFolder $_.destination -filter $_.filter
+}
